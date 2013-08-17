cc3c0fef2dc799c17308bacc966a2ff6f1894a6d
alistairjcbrown <github@alistairjcbrown.com>
2013-08-17 23:59:09 +0200

    Adding test index page

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en-us">
+    <head>
+        <meta charset="utf-8">
+        <title><?php echo $_SERVER['HTTP_HOST']; ?></title>
+        <meta name="viewport" content="width=device-width">
+    </head>
+    <body>
+        <h1><?php echo $_SERVER['HTTP_HOST']; ?></h1>
+        <p>Enabled sites alistairjcbrown.com</p>
+    </body>
+</html>
